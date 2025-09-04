d66b026538bc3dae47ca9474882190bd0b9ee36d
Ania-beigi <anahita.einbeigi@gmail.com>
2025-09-04 17:11:22 +0200

    Use NEXT_PUBLIC_ env variables for Contentful

--- src/lib/contentfulClient.ts
+++ src/lib/contentfulClient.ts
@@ -23,9 +23,13 @@
 
 import { createClient } from 'contentful';
 
+// export const client = createClient({
+//   space: process.env.CONTENTFUL_SPACE_ID as string,
+//   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+// });
 export const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID as string,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID as string,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN as string,
 });
 
 export const fetchEntries = async () => {
